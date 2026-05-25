376e91e3631c6971d6b49f481a65c8de6dc51a35
nesya raya <nesyaraya159@gmail.com>
2026-05-25 10:44:42 +0700

    chart

--- public/js/chart1-admin.js
+++ public/js/chart1-admin.js
@@ -1,21 +1,36 @@
 var ctx = document.getElementById('myChart').getContext('2d');
 var myChart = new Chart(ctx, {
-    type: 'line',
+    type: 'doughnut',
     data: {
         labels: ['Jan', 'Feb', 'Mar', 'Apr', 'Mei', 'Jun', 'Jul', 'Agt', 'Sep', 'Okt', 'Nov', 'Des'],
         datasets: [{
-            label: 'Jumlah Peminjam',
+            label: 'Jumlah peminjam',
             data: [55, 30, 70, 100, 90, 60, 80, 45, 25, 10, 65, 20],
             backgroundColor: [
-                'black'
+                '#BFDDF0',
+                '#F6F4E8',
+                '#FFEEA9'
             ],
+
             borderColor: [
-                'black'
+                '#EFF3EA',
+                '#C5D3E8',
+                '#FFEEA8'
             ],
             borderWidth: 1
         }]
     },
     options: {
-        responsive:true
+        responsive: true,
+        maintainAspectRatio: false,
+
+        cutout: '50%',
+
+        plugins: {
+            legend: {
+                position: 'top'
+            }
+        }
+
     }
 });
